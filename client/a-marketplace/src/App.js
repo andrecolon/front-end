5c70da43f487a1911485863b6aca5b6f30c35755
Steven Craig Moss <sc.moss@outlook.com>
2020-06-19 21:58:37 -0400

    CO-Author: Janneth Williams <built components out for Advanced React functionality to begin development>

--- client/a-marketplace/src/App.js
+++ client/a-marketplace/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login' 
-
+import Login from './components/Login' ;
+import SignUp from './components/SignUp';
+import AddItems from './components/AddItems';
+import ListPage from './components/ListPage';
 
 function App() {
   return (
     <div className="App">
       <Login /> 
+      <SignUp />
+      <AddItems />
+      {/* the ListPage component will have a Header component within ListPage.js */}
+      <ListPage />
     </div>
   );
 }
--- client/a-marketplace/src/components/Login.js
+++ client/a-marketplace/src/components/Login.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React,  { useState }from 'react';
 import {Link, Route} from 'react-router-dom';
-import {Button, Form, Card, CardImg, FormGroup, Input, Label } from 'reactstrap'
+import {Button, Form, Card, CardImg, FormGroup, Input, Label } from 'reactstrap';
+import *as yup from 'yup';
+import axios from 'axios';
 
 const Login = () => {
+    const [loginData, setloginData] = useState ({
+        name: "",
+        password: ""
+    })
+    const schema = yup.object().shape({
+        name: yup.string().required().min(2),
+        password: yup.string().required().min(1)
+    })
+    const login = () => {
+        schema.validate(loginData).then( () => {
+            axios.post('https://amp-node-api.herokuapp.com/api/auth/login', loginData).then((res) => {
+                console.log(res.data, "This is the posted data")
+            })
+        })
+    } 
+    const handleChange = (e) => {
+        setloginData({...loginData, [e.target.name]: e. target.value})
+    }
     return(
         <>
             <Form  style={{width: '50%', margin:'0 auto', border:'2px solid black', marginTop: '10px', backgroundColor:'#303030', color:'white'}}>
